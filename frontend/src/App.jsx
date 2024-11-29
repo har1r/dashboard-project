dfdc55a49bdb01f8fd2f5ed93b8d406cd5dc9889
Astukalyana <astukalyana@gmail.com>
2024-11-29 11:17:39 +0700

    Membuat routes untuk DashboardPage, LandingPage, LoginPage, dan NopelPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+// Import halaman disini
+import DashboardPage from './pages/DashboardPage';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import NopelPage from './pages/NopelPage';
+// -----------------
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="flex flex-col min-h-screen bg-gradient-to-br from-[#ffe0bf] to-[#f8e1a1]">
+        <main className="flex-grow container mx-auto px-4 py-8 xs:px-1 xs:w-full">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="dashboard" element={<DashboardPage />} />
+            <Route path="/masuk" element={<LoginPage />} />
+            <Route path="/nopel" element={<NopelPage />} />
+          </Routes>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
-
-export default App
